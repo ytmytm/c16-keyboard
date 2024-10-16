196207b856c598523bfce7d37a7a1d9b375ef985
Maciej Witkowiak <mwitkowiak@gmail.com>
2024-10-16 22:53:44 +0200

    C64 firmware

---
+++ SEEED/C64_keyboard/C64_keyboard.ino
@@ -0,0 +1,221 @@
+
+// IDE: Seeeduino XIAO SAMD
+
+/*
+  C64keyboard - Commodore Keyboard library
+
+  Copyright (c) 2022 Hartland PC LLC
+  Written by Robert VanHazinga
+
+  Modified for C128 by Maciej 'YTM/Elysium' Witkowiak
+
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+#include "C64keyboard.h"
+
+
+#include <KeyboardController.h>
+
+
+// Initialize USB Controller
+USBHost usb;
+
+// Attach keyboard controller to USB
+KeyboardController keyboard(usb);
+
+
+C128keyboard *ckey;
+static volatile uint8_t kc, mod, modbuff;
+static volatile uint16_t kc_mapped;
+static volatile uint8_t keymapnum = 0; // current keymap number
+
+static volatile bool kpress=false;
+static volatile bool lshift=false, rshift=false, shiftlock=false, virtshift=false; // 4 shift states: left, right, lock(left), virtual (from mapping)
+
+static volatile unsigned long lastPressedMillis = 0, currentMillis = 0;
+constexpr const unsigned long inactivePeriodMillis = 5000; // keep the message on OLED for 5 seconds after a keypress
+
+//////////////////////////////////////////////////////////////
+// I2C
+#include <Wire.h>
+
+//////////////////////////////////////////////////////////////
+// OLED display
+// 128x32
+// https://github.com/olikraus/u8g2
+
+#include <U8g2lib.h>
+
+U8G2_SSD1306_128X32_UNIVISION_1_HW_I2C u8g2(U8G2_R0);
+
+//////////////////////////////////////////////////////////////
+
+void next_keymap() {
+    keymapnum++;
+    if (keymapnum == ckey->getNumKeyMaps()) keymapnum = 0;
+    displayKeyMap();
+}
+
+// This function intercepts key press
+void keyPressed() {
+  lastPressedMillis = currentMillis;
+  kc = keyboard.getOemKey();
+  mod = keyboard.getModifiers();
+  kpress = true;
+  kc_mapped = ckey->mapkeycode(kc,mod);
+  displayKey();
+
+  // handle special key functions
+  switch (kc_mapped) {
+    case IGNORE_KEYCODE:
+      break;
+    case KEY_MT_RESET:
+      resetMT88();
+      ckey->clearAllKeys();
+      displayState("reset MT");
+      break;
+    case KEY_KEYMAP:
+      next_keymap();
+      break;
+    case KEY_SHIFT_LOCK:
+      shiftlock = !shiftlock;
+      ckey->c64key(KEY_LSHIFT, shiftlock);
+      if (shiftlock) {
+        displayState("shift lock on");
+      } else {
+        displayState("shift lock off");
+      }
+      break;
+    case KEY_RESTORE:
+      digitalWrite (NMI_PIN, HIGH);
+      displayState("NMI");
+      break;
+    default:
+      ckey->c64key(kc_mapped, kpress);
+      break;
+  }
+}
+
+// This function intercepts key release
+void keyReleased() {
+  lastPressedMillis = currentMillis;
+  kc = keyboard.getOemKey();
+  mod = keyboard.getModifiers();
+  kpress = false;
+  kc_mapped = ckey->mapkeycode(kc,mod);
+  displayKey();
+
+  // handle special key functions
+  switch (kc_mapped) {
+    case IGNORE_KEYCODE:
+      break;
+    case KEY_RESTORE:
+      digitalWrite (NMI_PIN, LOW);
+      break;
+    default:
+      ckey->c64key(kc_mapped, kpress);
+      break;
+  }
+}
+
+//////////////////////////////////////////////////////////////
+
+void displayKeyMap() {
+  u8g2.firstPage();
+  do {
+   u8g2.setFlipMode(true);
+   u8g2.setFont(u8g2_font_7x13B_tr);
+   u8g2.setCursor(0,14);
+   String s;
+   s = "New keymap: " + String(keymapnum);
+   u8g2.print(s);
+   u8g2.setCursor(0,31);
+   s = ckey->getCurrentKeyMapName();
+   u8g2.print(s);
+  } while ( u8g2.nextPage() );
+}
+
+
+void displayKey() {
+  u8g2.firstPage();
+  do {
+   u8g2.setFlipMode(true);
+   u8g2.setFont(u8g2_font_7x13B_tr);
+   u8g2.setCursor(0,14);
+   String s;
+   s = "Key: " + String(kc,HEX) + " Mod: " + String(mod, HEX);
+   u8g2.print(s);
+   u8g2.setCursor(0,31);
+   s = "map:"+String(kc_mapped, HEX) + " " + String(get_mapped_key_name(kc_mapped));
+   u8g2.print(s);
+  } while ( u8g2.nextPage() );
+}
+
+
+void displayState(const char* s) {
+  u8g2.firstPage();
+  do {
+   u8g2.setFlipMode(true);
+   u8g2.setFont(u8g2_font_7x13B_tr);
+   u8g2.setCursor(0,14);
+   u8g2.print(s);
+  } while ( u8g2.nextPage() );
+}
+
+//////////////////////////////////////////////////////////////
+
+void setup() {
+  ckey = new C128keyboard();
+
+  u8g2.begin();
+  Wire.begin();
+  usb.Init();
+
+  displayState("Hello world!");
+
+  ckey->clearAllKeys(); // just in case RESET line is not used
+}
+
+uint32_t lastUSBstate = 0;
+
+void loop() {
+  currentMillis = millis();
+  if (currentMillis - lastPressedMillis > inactivePeriodMillis) {
+    // clear screen if there was no activity for inactivePeriodMillis
+    u8g2.firstPage();
+    do { } while ( u8g2.nextPage() );
+    lastPressedMillis = currentMillis; // prevent screen clearing and i2c access in every following loop during inactivity
+  }
+
+  // Process USB tasks
+  usb.Task();
+
+  uint32_t currentUSBstate = usb.getUsbTaskState();
+  String state;
+  if (lastUSBstate != currentUSBstate) {
+    switch (currentUSBstate) {
+      case USB_ATTACHED_SUBSTATE_SETTLE: state="Device Attached"; break;
+      case USB_DETACHED_SUBSTATE_WAIT_FOR_DEVICE: state="Detached, waiting for Device"; break;
+      case USB_ATTACHED_SUBSTATE_RESET_DEVICE: state="Resetting Device"; break;
+      case USB_ATTACHED_SUBSTATE_WAIT_RESET_COMPLETE: state="Reset complete"; break;
+      case USB_STATE_CONFIGURING: state="USB Configuring"; break;
+      case USB_STATE_RUNNING: state="USB Running"; break;
+    }
+    displayState(state.c_str());
+    lastUSBstate = currentUSBstate;
+  }
+}
---
+++ SEEED/C64_keyboard/keymapping.h
@@ -0,0 +1,105 @@
+
+/* Maps are positional in relation to keyboard USB codes.
+Place C64 key byte (Chart is found in KEYBIN spreadsheet)
+into the equivalent keyboard USB code code position.
+For C64 keyboards normally USB code set 2.
+
+Each map has a normal (without shift) and shifted (with shift) array.
+
+In situations where there is a shift difference, add 64 to the C64 key byte. 
+This is when a key is shifted on keyboard, but not shifted on the C64 or vise-versa.
+
+Unused USB codes need to have the IGNORE_KEYCODE value in place.(Default 170)
+
+https://deskthority.net/wiki/Scancode#USB_HID_Usage_Codes
+
+https://sites.google.com/site/h2obsession/CBM/C128/keyboard-scan
+
+*/
+
+//  Modified for C128 by Maciej 'YTM/Elysium' Witkowiak
+
+// order of this table is so weird (if you look into C64/C128 keyboard matrix) because
+// of the way how original (PS/2) code had MTxxxx lines attached
+// it's [AX0..2,AY0..2] (AX*8+AY) but 3 bits of AX and 3 bits of AY are both in reverse order
+// there are 6 bits of key address total,
+// AX3 is used for logic table gap on MT8812/16 for X6/X7 and for C128 keys (lines X8/X9/X10)
+// anything above $00ff is a special modifier or action key
+
+enum C128_Key {
+   // C64 8x8 keymap
+   KEY_DEL = 0x00           // 0 DEL
+  ,KEY_F1 = 0x01            // 1 F1
+  ,KEY_HORZ_CSR = 0x02      // 2 HORZ CSR
+  ,KEY_F5 = 0x03            // 3 F5
+  ,KEY_RETURN = 0x04        // 4 RETURN
+  ,KEY_F3 = 0x05            // 5 F3
+  ,KEY_F7 = 0x06            // 6 F7
+  ,KEY_VERT_CSR = 0x07      // 7 VERT CSR
+  ,KEY_9 = 0x08             // 8 9
+  ,KEY_M = 0x09             // 9 M
+  ,KEY_J = 0x0A             // 10 J
+  ,KEY_O = 0x0B             // 11 O
+  ,KEY_I = 0x0C             // 12 I
+  ,KEY_K = 0x0D             // 13 K
+  ,KEY_ZERO = 0x0E          // 14 0
+  ,KEY_N = 0x0F             // 15 N
+  ,KEY_5 = 0x10             // 16 5
+  ,KEY_C = 0x11             // 17 C
+  ,KEY_D = 0x12             // 18 D
+  ,KEY_T = 0x13             // 19 T
+  ,KEY_R = 0x14             // 20 R
+  ,KEY_F = 0x15             // 21 F
+  ,KEY_6 = 0x16             // 22 6
+  ,KEY_X = 0x17             // 23 X
+  ,KEY_POUND = 0x18         // 24 POUND
+  ,KEY_RSHIFT = 0x19        // 25 RSHIFT
+  ,KEY_SEMICOLON = 0x1A     // 26 ;
+  ,KEY_ARR_UP = 0x1B        // 27 ^
+  ,KEY_STAR = 0x1C          // 28 *
+  ,KEY_EQUALS = 0x1D        // 29 =
+  ,KEY_HOME = 0x1E          // 30 HOME
+  ,KEY_SLASH = 0x1F         // 31 /
+  ,KEY_3 = 0x20             // 32 3
+  ,KEY_Z = 0x21             // 33 Z
+  ,KEY_A = 0x22             // 34 A
+  ,KEY_E = 0x23             // 35 E
+  ,KEY_W = 0x24             // 36 W
+  ,KEY_S = 0x25             // 37 S
+  ,KEY_4 = 0x26             // 38 4
+  ,KEY_LSHIFT = 0x27        // 39 LSHIFT
+  ,KEY_PLUS = 0x28          // 40 +
+  ,KEY_DOT = 0x29           // 41 .
+  ,KEY_L = 0x2A             // 42 L
+  ,KEY_AT = 0x2B            // 43 @
+  ,KEY_P = 0x2C             // 44 P
+  ,KEY_COLON = 0x2D         // 45 :
+  ,KEY_MINUS = 0x2E         // 46 -
+  ,KEY_COMMA = 0x2F         // 47 ,
+  ,KEY_7 = 0x30             // 48 7
+  ,KEY_B = 0x31             // 49 B
+  ,KEY_G = 0x32             // 50 G
+  ,KEY_U = 0x33             // 51 U
+  ,KEY_Y = 0x34             // 52 Y
+  ,KEY_H = 0x35             // 53 H
+  ,KEY_8 = 0x36             // 54 8
+  ,KEY_V = 0x37             // 55 V
+  ,KEY_1 = 0x38             // 56 1
+  ,KEY_SPACE = 0x39         // 57 SPACE
+  ,KEY_CTRL = 0x3A          // 58 CTRL
+  ,KEY_Q = 0x3B             // 59 Q
+  ,KEY_ARR_LEFT = 0x3C      // 60 <-
+  ,KEY_CBM = 0x3D           // 61 CBM
+  ,KEY_2 = 0x3E             // 62 2
+  ,KEY_RUN_STOP = 0x3F      // 63 RUN/STOP
+
+  // modifiers
+  ,MOD_SHIFT = 0x100        // virtual shift modifier (add shift on base map, remove shift on shifted map) (this is used as a bit modifier)
+  // special actions
+  ,KEY_RESTORE = 0x400      // special action: RESTORE (NMI)
+  ,KEY_KEYMAP = 0x401       // special action: switch to next keymap
+  ,KEY_MT_RESET = 0x402     // special action: reset MT88XX chip
+  ,KEY_SHIFT_LOCK = 0x403   // special action: shift lock (left shift toggle)
+  //
+  ,IGNORE_KEYCODE = 0xFFF   // special: ignore keycode
+};
